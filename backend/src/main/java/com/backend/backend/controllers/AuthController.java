585bfc8b78bdb287401083e249199fe0bdf7a62f
Saracaypao <paola.saracay@hotmail.com>
2025-06-14 23:23:00 -0600

    auth controller

---
+++ backend/src/main/java/com/backend/backend/controllers/AuthController.java
@@ -0,0 +1,23 @@
+package com.backend.backend.controllers;
+
+import com.backend.backend.dto.UserLoginDTO;
+import com.backend.backend.dto.UserResponseDTO;
+import com.backend.backend.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDTO> login(@RequestBody UserLoginDTO dto) {
+        return ResponseEntity.ok(authService.login(dto));
+    }
+}
